48ee8060cf56f146348ef8663386f920472e7c4e
cmdblock <151073324+pokitest@users.noreply.github.com>
2023-11-17 19:33:57 -0800

    removed the weird black lines

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 async function getgames(){return JSON.parse(await (await fetch("/games.json")).text())};
-function makebutton(e){newbutton=document.createElement("a"),document.body.append(newbutton),newbutton.innerHTML=' <a class="'+e+'" rel="noreferrer noopener" target="_blank" href="/games/'+e+'/game"><img src="/games/'+e+'/icon" width="50" height="50"></img> </a>'};
+function makebutton(e){newbutton=document.createElement("a"),document.body.append(newbutton),newbutton.innerHTML=' <a class="'+e+'" rel="noreferrer noopener" target="_blank" href="/games/'+e+'/game"><img src="/games/'+e+'/icon" width="50" height="50"></img></a>'};
 
 getgames().then(e=>{for(let t=0;t<e.length;t++)makebutton(e[t])});
