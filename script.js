e1d85b3629ad8691daa17d4c56efe8415ffcf4e8
cmdblock <151073324+pokitest@users.noreply.github.com>
2023-11-17 23:36:29 -0800

    small ui changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 async function getgames(){return JSON.parse(await (await fetch("/games.json")).text())};
-function makebutton(e){newbutton=document.createElement("a"),document.body.append(newbutton),newbutton.outerHTML=' <a class="'+e+'" rel="noreferrer noopener" target="_blank" href="/games/'+e+'/game"><img alt="'+e+'" style="border-radius: 30%;" src="/games/'+e+'/icon" width="100" height="100"></img></a>'};
+function makebutton(e){newbutton=document.createElement("a"),document.body.getElementsByClassName('container')[0].append(newbutton),newbutton.outerHTML=' <a class="'+e+'" rel="noreferrer noopener" target="_blank" href="/games/'+e+'/game"><img alt="'+e+'" style="border-radius: 30%;" src="/games/'+e+'/icon" width="100" height="100"></img></a>'};
 
 getgames().then(e=>{for(let t=0;t<e.length;t++)makebutton(e[t])});
